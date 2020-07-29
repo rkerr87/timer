b4a0b4b12677bb90a48025b92aee74ed6f25e2da
Ryan Kerr <rkerr87@gmail.com>
2020-07-29 13:08:32 -0700

    html changes and stylesheet

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-console */
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
+  onStart(totalDuration) {
+    duration = totalDuration
     console.log('Timer Started');
   },
-  onTick() {
-    console.log('Timer just ticked down');
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset', (perimeter * timeRemaining) / duration - perimeter);
   },
   onComplete() {
     console.log('Timer just completed');
